5560cb3b2040bd3716aab03155fde85a4a28dbeb
Wooksang Ryu <today8934@gmail.com>
2022-06-25 12:54:11 +0900

    4.1.3 멤버 가시성

--- src/main/kotlin/chapter4/Person.kt
+++ src/main/kotlin/chapter4/Person.kt
@@ -27,6 +27,16 @@ class Person(val fullName: String /* 주생성자 파라미터*/) {
 
 }
 
+fun main() {
+    val person5 = Person5("John", "Doe")
+
+    //println(person5.firstName) Person5 클래스에서 firstName과 familyName은 private이다.
+    //println(person5.familyName)
+    println(person5.fullName())
+
+    //val empty = Empty() Empty 클래스의 주생성사자 private이기때문에 init할 수 없다.
+}
+
 class Room(vararg val persons: Person /* 함수와 마찬가지로 디폴트 값과 vararg 를 생성자 파라미터에 사용할 수 있다. */) {
     fun showNames() {
         for (person in persons) println(person.fullName)
@@ -64,4 +74,21 @@ class Person3 {
 
 class Person4(val fullName: String) { /* Person3의 두번째 생성자를 주생성자처럼 쓸수도 있다. */
     constructor(firstName: String, familyName: String): this("$firstName $familyName")
+}
+
+/*
+다음 코드에서 firstName과 familyName은 private이므로 main() 함수에서 이 둘을 볼 수 없다. 반면 fullName()은 public이다.
+ */
+class Person5(
+    private val firstName: String,
+    private val familyName: String
+    ) {
+    fun fullName() = "$firstName $familyName"
+}
+
+/*
+주생성자의 가시성을 지정하려면 constructor 키워드를 꼭 명시해야 한다.
+ */
+class Empty private constructor() {
+    fun showMe() = println("Empty")
 }
