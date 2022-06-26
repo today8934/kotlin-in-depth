4cfd795b241b1071cb998d37d69123887edaa06c
Wooksang Ryu <today8934@gmail.com>
2022-06-26 17:31:23 +0900

    5.1.3 람다와 익명 함수

--- src/main/kotlin/chapter5/Chapter5.kt
+++ src/main/kotlin/chapter5/Chapter5.kt
@@ -18,6 +18,12 @@ fun main() {
 
     println(evalAtZero { n -> n + 1})
     println(evalAtZero { n -> n - 1})
+
+    println(check("Hello") { c -> c.isLetter() })
+    println(check("Hello") { it.isLowerCase() }) //람다 인자가 하나인 경우에는 파라미터 목록과 화살표 기호를 생략하고 it라는 이름을 사용하여 기리킬 수 있다.
+
+    println(check2("Hello") { _, c -> c.isLetter() }) //사용하지 않는 람다 파라미터를 _로 지정할 수 있다.
+    println(check2("Hello") { i, c -> i == 0 || c.isLowerCase() })
 }
 
 /*
@@ -35,9 +41,22 @@ fun aggregate(numbers: IntArray, op: (Int, Int) -> Int): Int {
 /*
 result와 op는 함수 파라미터 역할을 하며 -> 다음에 오는 식은 결과를 계산하는 식이다.
  */
-fun sum(numbers: IntArray) = aggregate(numbers) { result, op -> result + op }
+fun sum(numbers: IntArray) = aggregate(numbers) { result, op -> result + op } //람다식(파라미터: result, op 본문: result + op)
 
 fun max(numbers: IntArray) = aggregate(numbers) { result, op -> if (op > result) op else result}
+
+/*
+익명 함수
+- 익명 함수에는 이름을 지정하지 않는다. fun키워드 다음에 바로 파라미터 목록이 온다.
+- 람다와 마찬가지로 문맥에서 파라미터 타입을 추론할 수 있으면 파라미터 타입을 지정하지 않아도 된다.
+함수 정의와 달리, 익명 함수는 식이기 때문에 인자로 함수에 넘기거나 변수에 대입하는 등 일반 값처럼 쓸 수 있다.
+ */
+fun sum2(numbers: IntArray) = aggregate(numbers, fun(result, op) = result + op)
+
+/*
+익명함수 본문이 블록인 경우 명시적으로 반환 타입을 지정해야 한다.
+ */
+fun sum3(numbers: IntArray) = aggregate(numbers, fun(result, op): Int {return result + op})
 
 /*
 함수가 인자를 받지 않는 경우에는 함수 타입의 파라미터 목록에 빈 괄호를 사용한다.
@@ -57,4 +76,29 @@ fun measureTime2(action: (() -> Unit)?): Long {
     val start = System.nanoTime()
     action?.invoke()
     return System.nanoTime() - start
+}
+
+fun check(s: String, condition: (Char) -> Boolean): Boolean {
+    for (c in s) {
+        if (!condition(c)) return false
+    }
+    return true
+}
+
+fun check2(s: String, condition: (Int, Char) -> Boolean): Boolean {
+    for (i in s.indices) {
+        if (!condition(i, s[i])) return false
+    }
+    return true
+}
+
+/*
+람다나 익명 함수도 클로저 또는 자신을 포함하는 외부 선언에 정의된 변수에 접근할 수 있다.
+특히 람다나 익명 함수도 외부 영역의 가변 변수 값을 변경할 수 있다.
+반면 자바 람다는 외부 변수의 값을 변경할 수 없다.
+ */
+fun forEach(a: IntArray, action: (Int) -> Unit) {
+    for (n in a) {
+        action(n)
+    }
 }
